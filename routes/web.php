89b0b82a1f15d834e8228afbd8b2bcfa0af36f6e
Mamo09 <96432291+Mamo09@users.noreply.github.com>
2024-09-08 12:44:24 +0700

    update blog post

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,23 @@ Route::get('/about', function () {
     return view('about', ['nama'=>'Futma'],['title'=>'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title'=>'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title'=>'Blog', 'posts'=>[
+        [
+            'title' => 'Manfaat Olahraga Pagi untuk Kesehatan',
+            'author' => 'Futma Nurhidayat',
+            'body' => 'Olahraga pagi memberikan banyak manfaat untuk kesehatan fisik dan mental. Saat tubuh aktif di pagi hari, metabolisme menjadi lebih cepat dan tubuh membakar kalori dengan lebih efisien sepanjang hari. Selain itu, olahraga pagi dapat meningkatkan suasana hati karena produksi endorfin yang membuat tubuh merasa lebih bahagia dan berenergi.
+            Tidak hanya bermanfaat bagi fisik, olahraga pagi juga bisa membantu menjaga kesehatan mental. Mengawali hari dengan aktivitas fisik terbukti mengurangi tingkat stres dan kecemasan. Dengan suasana hati yang lebih baik, seseorang bisa lebih fokus dan produktif dalam menjalani aktivitas harian.'
+        ],
+
+        [
+            'title' => 'Pentingnya Pemrograman dalam Era Digital',
+            'author' => 'Futma Nurhidayat',
+            'body' => '<p>Pemrograman kini menjadi keterampilan yang sangat penting di era digital. Dengan kemampuan pemrograman, seseorang bisa menciptakan aplikasi, website, atau software yang memudahkan kehidupan sehari-hari. Selain itu, banyak industri yang kini sangat bergantung pada teknologi dan memerlukan programmer untuk mengembangkan solusi digital.
+            Di dunia yang terus berkembang ini, keterampilan dalam pemrograman juga membuka banyak peluang karir. Profesi seperti web developer, data scientist, hingga software engineer kini menjadi pekerjaan yang sangat dicari. Menguasai pemrograman tidak hanya menawarkan keamanan karir, tetapi juga memberikan kemampuan untuk berinovasi di bidang teknologi.'
+        ]
+
+    ]]);
 });
 
 Route::get('/contact', function () {
