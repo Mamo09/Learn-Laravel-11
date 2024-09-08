052f22c6f0986a7d649894434dc4ad84f598a068
Mamo09 <96432291+Mamo09@users.noreply.github.com>
2024-09-08 12:07:01 +0700

    UI Article update

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,25 +1,28 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    <h1 class="text-xl">Halaman Blog</h1>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Manfaat Olahraga Pagi untuk Kesehatan</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Futma Nurhidayat | 7 September 2024</a>
+        </div>
+        <p class="my-4 font-light">
+            Olahraga pagi memberikan banyak manfaat untuk kesehatan fisik dan mental. Saat tubuh aktif di pagi hari, metabolisme menjadi lebih cepat dan tubuh membakar kalori dengan lebih efisien sepanjang hari. Selain itu, olahraga pagi dapat meningkatkan suasana hati karena produksi endorfin yang membuat tubuh merasa lebih bahagia dan berenergi.
+        </p>
 
-    <h3 class="text-xl">Manfaat Olahraga Pagi untuk Kesehatan</h3>
-    <p class="text-s">Olahraga pagi memberikan banyak manfaat untuk kesehatan fisik dan mental. Saat tubuh aktif di pagi hari,
-        metabolisme menjadi lebih cepat dan tubuh membakar kalori dengan lebih efisien sepanjang hari. Selain itu,
-        olahraga pagi dapat meningkatkan suasana hati karena produksi endorfin yang membuat tubuh merasa lebih bahagia
-        dan berenergi.</p>
-    <p>Tidak hanya bermanfaat bagi fisik, olahraga pagi juga bisa membantu menjaga kesehatan mental. Mengawali hari
-        dengan aktivitas fisik terbukti mengurangi tingkat stres dan kecemasan. Dengan suasana hati yang lebih baik,
-        seseorang bisa lebih fokus dan produktif dalam menjalani aktivitas harian.</p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+    </article>
 
-    <h3>Pentingnya Pemrograman dalam Era Digital</h3>
-    <p>Pemrograman kini menjadi keterampilan yang sangat penting di era digital. Dengan kemampuan pemrograman, seseorang
-        bisa menciptakan aplikasi, website, atau software yang memudahkan kehidupan sehari-hari. Selain itu, banyak
-        industri yang kini sangat bergantung pada teknologi dan memerlukan programmer untuk mengembangkan solusi
-        digital.</p>
-    <p>Di dunia yang terus berkembang ini, keterampilan dalam pemrograman juga membuka banyak peluang karir. Profesi
-        seperti web developer, data scientist, hingga software engineer kini menjadi pekerjaan yang sangat dicari.
-        Menguasai pemrograman tidak hanya menawarkan keamanan karir, tetapi juga memberikan kemampuan untuk berinovasi
-        di bidang teknologi.</p>
+    
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2>Pentingnya Pemrograman dalam Era Digital</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Futma Nurhidayat | 8 September 2024</a> 
+        </div>
+        <p class="my-4 font-light">
+            Pemrograman kini menjadi keterampilan yang sangat penting di era digital. Dengan kemampuan pemrograman, seseorang bisa menciptakan aplikasi, website, atau software yang memudahkan kehidupan sehari-hari. Selain itu, banyak industri yang kini sangat bergantung pada teknologi dan memerlukan programmer untuk mengembangkan solusi digital.
+        </p>
 
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+    </article>
 </x-layout>
