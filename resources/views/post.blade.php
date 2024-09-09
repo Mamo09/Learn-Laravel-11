a623121d4cd49f6b5e21b77aea050723d9c938f4
Mamo09 <96432291+Mamo09@users.noreply.github.com>
2024-09-09 16:55:59 +0700

    eloquent ORM & Post Model

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a>
+            <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->format('j F Y') }}
         </div>
 
         <p class="my-4 font-light">
@@ -17,3 +17,4 @@
     </article>
 
 </x-layout>
+
